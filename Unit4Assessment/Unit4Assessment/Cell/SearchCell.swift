477f1a1d9de4bbff6e9052099c326531eda5c495
Margiett Gil <mxg627@icloud.com>
2020-02-11 16:46:35 -0500

    need to debug my createViewController started working on the search view controller and added the search cell.

---
+++ Unit4Assessment/Unit4Assessment/Cell/SearchCell.swift
@@ -0,0 +1,22 @@
+//
+//  SearchCell.swift
+//  Unit4Assessment
+//
+//  Created by Margiett Gil on 2/11/20.
+//  Copyright © 2020 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class SearchCell: UICollectionViewCell {
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+        label.font = UIFont.preferredFont(forTextStyle: .headline)
+        label.text = "first question"
+        return label
+    }()
+    
+    
+}
--- Unit4Assessment/Unit4Assessment/ViewControllers/SearchController.swift
+++ Unit4Assessment/Unit4Assessment/ViewControllers/SearchController.swift
@@ -10,22 +10,19 @@ import UIKit
 import DataPersistence
 
 class SearchController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  private var searchView = SearchView()
+    var dataPersistence: DataPersistence<FlashCardModel>!
+        
+        
+        private var flashCardsDidSetSearch = [FlashCardModel]() {
+            didSet{
+                DispatchQueue.main.async {
+                    self.searchView.collectionV.reloadData()
+                }
+            }
+        }
+        
+        
+       
 }
