f690f7b8e8eea1856a2eb0ccc9494d08b8e307bc
Margiett Gil <mxg627@icloud.com>
2020-02-11 16:56:18 -0500

    added gestures to the search cell

--- Unit4Assessment/Unit4Assessment/Cell/SearchCell.swift
+++ Unit4Assessment/Unit4Assessment/Cell/SearchCell.swift
@@ -18,5 +18,46 @@ class SearchCell: UICollectionViewCell {
         return label
     }()
     
+    private lazy var quizFactLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.font = UIFont.preferredFont(forTextStyle: .subheadline)
+        label.text = "second question"
+        return label
+    }()
+    
+    lazy var tapGesture: UITapGestureRecognizer = {
+       let gesture = UITapGestureRecognizer()
+        gesture.addTarget(self, action: #selector(cellWasTapped))
+        return gesture
+    }()
+    
+    private var isBackOfCardShowing = false
+    
+    @objc private func cellWasTapped(_ gesture: UITapGestureRecognizer){
+           if gesture.state == .began || gesture.state == .changed {
+               return
+           }
+           isBackOfCardShowing.toggle()
+           animate()
+       }
+       
+       private func animate(){
+           let duration = 1.0
+           
+           if isBackOfCardShowing {
+               UIView.transition(with: self, duration: duration, options: [.transitionFlipFromRight], animations: {
+                   self.quizFactLabel.alpha = 1
+                   self.titleLabel.alpha = 0
+               }, completion: nil)
+           } else {
+               UIView.transition(with: self, duration: duration, options: [.transitionFlipFromLeft], animations: {
+                   self.quizFactLabel.alpha = 0
+                   self.titleLabel.alpha = 1
+               }, completion: nil)
+           }
+       }
+    
+    
     
 }
